cc6b0ee63efe3838438eb130183a97b4504aa6a8
JavaJunkie1337 <156255455+JavaJunkie1337@users.noreply.github.com>
2024-01-11 23:48:21 -0500

    this is test.java

---
+++ test.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void main (String[] args) {
+        Scanner scanner = new
+        Scanner(System.in);
+        System.out.print("Enter some text: ");
+        String userInput =
+        scanner.nextLine();
+        int value = 0;
+        while (value < 1) {
+        System.out.println(userInput);
+            System.out.println(" ");
+            scanner.close();
+        }
+    }
